295f5c99d9bf90442a3d82fc3fc31d061f718a26
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-03-11 15:35:07 +0700

    Update seeder

--- src/database/seeds/BantenprovVueOpdSeederVueOpd.php
+++ src/database/seeds/BantenprovVueOpdSeederVueOpd.php
@@ -14,34 +14,19 @@ class BantenprovVueOpdSeederVueOpd extends Seeder
      *
      * @return void
      */
-	public function run()
-	{
+    public function run()
+    {
         Model::unguard();
 
+        $model      = new VueOpd;
         $file       = file_get_contents(public_path('json/bantenprov/vue-opd/vue-opd-data.json'));
         $json       = json_decode($file, true);
         $vue_opds   = array_get($json, '2.data');
 
-        foreach ($vue_opds as $vue_opd) {
-            $model = VueOpd::updateOrCreate(
-                [
-                    'id' => $vue_opd['id'],
-                ],
-                [
-                    'id'            => $vue_opd['id'],
-                    'kunker'        => $vue_opd['kunker'],
-                    'name'          => $vue_opd['name'],
-                    'kunker_sinjab' => $vue_opd['kunker_sinjab'],
-                    'kunker_simral' => $vue_opd['kunker_simral'],
-                    'levelunker'    => $vue_opd['levelunker'],
-                    'njab'          => $vue_opd['njab'],
-                    'npej'          => $vue_opd['npej'],
-                    '_lft'          => $vue_opd['_lft'],
-                    '_rgt'          => $vue_opd['_rgt'],
-                    'parent_id'     => $vue_opd['parent_id'],
-                ]
-            );
-            $model->save();
-        }
-	}
+        $model::truncate();
+
+        DB::table($model->getTable())->insert(
+            $vue_opds
+        );
+    }
 }
